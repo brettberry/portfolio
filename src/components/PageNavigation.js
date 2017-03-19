44d51fbdca19b590b7c39ea6c7296b21ab7b243b
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-03-18 23:02:03 -0700

    Switched out page navigation for mobile.

--- src/components/PageNavigation.js
+++ src/components/PageNavigation.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FaGithub from 'react-icons/lib/fa/github';
 import { Link as ScrollLink } from 'react-scroll';
 import './pageNavigation.styles.scss';
 
-function PageNavigation() {
-    return (
-        <div className="container">
+class PageNavigation extends Component {
+
+    state = {
+        isDesktop: true
+    };
+
+    constructor(props) {
+        super(props);
+        this.handleResize = () => this.setState({ isDesktop: matchMedia('(min-width: 500px)').matches });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {this.state.isDesktop ? this.renderDesktopNavigation() : this.renderMobileNavigation()}
+                <div className="horizontalRule"/>
+            </div>
+        );
+    }
+
+    renderDesktopNavigation() {
+        return (
             <div className="linksContainer">
                 <ScrollLink to="freelance" smooth={true} duration={800}>
                     <h3 className="pageNavigation">Freelance</h3>
@@ -23,9 +50,27 @@ function PageNavigation() {
                     <FaGithub className="githubIcon"/>
                 </a>
             </div>
-            <div className="horizontalRule"/>
-        </div>
-    );
+        );
+    }
+
+    renderMobileNavigation() {
+        return (
+            <div className="linksContainer">
+                <ScrollLink to="freelance" smooth={true} duration={800}>
+                    <h3 className="pageNavigation">Freelance</h3>
+                </ScrollLink>
+                <ScrollLink to="hobby" smooth={true} duration={800}>
+                    <h3 className="pageNavigation">Projects</h3>
+                </ScrollLink>
+                <ScrollLink to="education" smooth={true} duration={800}>
+                    <h3 className="pageNavigation">About</h3>
+                </ScrollLink>
+                <a target="_blank" href="https://github.com/brettberry">
+                    <FaGithub className="githubIcon"/>
+                </a>
+            </div>
+        );
+    }
 }
 
 export default PageNavigation;
